cfa1487c5cbda030927e7cb06ad3c891ca69d350
Travis Lee <travisslee223@gmail.com>
2020-10-23 19:55:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,6 +47,7 @@ class ApplicationController < Sinatra::Base
 
   delete '/articles/:id/delete' do
     @article = Article.find(params["id"])
+    binding.pry
     @article.destroy
     redirect '/articles'
   end
